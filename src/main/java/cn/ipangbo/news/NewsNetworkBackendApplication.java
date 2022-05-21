42981ada54944e49f9fca44792002a106d9fc1a6
ipangbo <122402331@qq.com>
2022-05-21 22:24:46 +0800

    Initial commit

---
+++ src/main/java/cn/ipangbo/news/NewsNetworkBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.ipangbo.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsNetworkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsNetworkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ipangbo/news/NewsNetworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ipangbo.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsNetworkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
